89473c0d65c1566e03711677846665cd742dae18
Karol Filas <filaskarol@gmail.com>
2023-04-16 09:05:35 +0200

    second example

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
-const add = (start = 0) => {
-    let number = start
-    return () => {
-        number++
-        document.body.textContent = `aktulany stan licznika kliknięc to ${number}`
+// const add = (start = 0) => {
+//     let number = start
+//     return () => {
+//         number++
+//         document.body.textContent = `aktulany stan licznika kliknięc to ${number}`
+//     }
+// }
+
+// const counter = add()
+// document.addEventListener('click', counter)
+
+const user = (name = '', age) => {
+    let userName = name
+    let userAge = age
+    function showName() {
+        console.log(`Cześć ${userName}, ${userAge > 18 ? " możesz kupić piwo" : "nie możesz kupić piwa"}`);
     }
+    return showName
 }
 
-const counter = add()
-document.addEventListener('click', counter)
+const mieszko = user('mieszko', 22)
+const jan = user('jan', 12)
+mieszko()
+jan()
